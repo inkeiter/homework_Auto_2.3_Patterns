28023236870bb8400185de9c9123068158d047eb
Kate Popova <popova1919@yandex.ru>
2025-08-27 20:19:32 +0530

    Init commit

---
+++ src/main/java/ru/netology/delivery/DataGenerator.java
@@ -0,0 +1,60 @@
+//Утилитный класс-содержит приватный конструктор и статичные методы.
+//Используем для удобства. Вызвали 1 метод, который сгенерировал сразу 3 поля.
+
+package ru.netology.delivery;
+
+import net.datafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
+        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
+        LocalDate date = LocalDate.now().plusDays(shift);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return date.format(formatter);
+    }
+
+    public static String generateCity(Faker faker) {
+        return faker.address().cityName();
+    }
+
+    public static String generateName(Faker faker) {
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone(Faker faker) {
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private static Faker faker;
+
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            faker = new Faker(new Locale(locale));
+            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(faker),
+            // generateName(faker), generatePhone(faker)
+            String city = generateCity(faker);
+            String name = generateName(faker);
+            String phone = generatePhone(faker);
+            return new UserInfo(city, name, phone);
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
